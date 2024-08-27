5c22159e1409947661f386bf140ff7bbe526a5ab
Mehmet E Belviranli <54647757+belviranli@users.noreply.github.com>
2024-08-27 12:01:22 -0600

    Create e1.c

---
+++ e1.c
@@ -0,0 +1,17 @@
+// Excercise 1: printf 
+
+#include <stdio.h>
+
+int main(int argc, void *argv[]){
+    // Print Hello 442
+
+    // Print integers
+    
+    // Print characters
+    
+    // Print hexadecimal
+
+    // Print floating point numbers
+
+    return 0;
+}
